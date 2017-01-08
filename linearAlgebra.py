c0df6ff601fb7ab3c566559ecf97bd878a178699
Sven ten Haaf <sventenhaaf@hotmail.com>
2017-01-08 14:45:43 +0100

    done with vector functions;

--- linearAlgebra.py
+++ linearAlgebra.py
@@ -2,4 +2,17 @@ def vector_add(v, w):
     return [v_i + w_i
             for v_i, w_i in zip(v, w)]
 
+def vector_sum(vectors):
+    return reduce(vector_add, vectors)
+
+def dot(v, w):
+    return sum(v_i * w_i
+               for v_i, w_i in zip(v, w))
+
 print vector_add([1, 2], [3, 4])
+
+print vector_sum([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+print dot([1, 2, 3], [4, 5, 6])
+
+print dot([1, 2, 3], [1, 2, 3])
